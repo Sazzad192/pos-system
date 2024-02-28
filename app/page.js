149f7779aaf8716ef26d4a40b3f6c722043a84b3
Sazzad192 <sazadulbg130@gmail.com>
2024-02-28 13:03:42 +0600

    add tailwind colors

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div id="pos" className="">
+    <div id="pos" className="text-primary-500">
       Hello this is POS System Application
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-50": "#eff6ff",
+        "primary-100": "#dbeafe",
+        "primary-200": "#bfdbfe",
+        "primary-300": "#93c5fd",
+        "primary-400": "#60a5fa",
+        "primary-500": "#3b82f6",
+        "primary-600": "#2563eb",
+        "primary-700": "#1d4ed8",
+        "secondary-50": "#f5f3ff",
+        "secondary-100": "#ede9fe",
+        "secondary-200": "#c7d2fe",
+        "secondary-300": "#a5b4fc",
+        "secondary-400": "#818cf8",
+        "secondary-500": "#6366f1",
+        "secondary-600": "#4f46e5",
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
